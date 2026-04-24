0277afdfc1bc949b5e7244aed92853a0c1d3e80c
Troy Simeon Taylor <44444967+troystaylor@users.noreply.github.com>
2026-04-24 13:04:44 -0400

    Switch from CopilotClient to Direct Line, fix MemoryStorage, add ServiceNow password grant

--- ServiceNow Handoff/ServiceNow/IServiceNowConnectionSettings.cs
+++ ServiceNow Handoff/ServiceNow/IServiceNowConnectionSettings.cs
@@ -1,11 +1,1 @@
-namespace ServiceNowHandoff.ServiceNow;
-
-public interface IServiceNowConnectionSettings
-{
-    string InstanceUrl { get; }
-    string ClientId { get; }
-    string ClientSecret { get; }
-    string WebhookSecret { get; }
-    string QueueId { get; }
-    int PollingIntervalSeconds { get; }
-}
+PLACEHOLDER
